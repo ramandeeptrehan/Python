e8643c441543c4c34656cec160005e4f073823e3
rtrehan <ramandeeptrehan@gmail.com>
2023-08-01 18:16:05 -0700

    Add comments to code

--- Learning-August-2023/main.py
+++ Learning-August-2023/main.py
@@ -18,6 +18,21 @@ def read_root():
     Link: https://coderslegacy.com/python/union-in-typing/
 '''
 
+'''
+
+Open your browser at http://127.0.0.1:8000/items/5?q=somequery.
+
+You will see the JSON response as:
+{"item_id": 5, "q": "somequery"}
+
+You already created an API that:
+
+Receives HTTP requests in the paths / and /items/{item_id}.
+Both paths take GET operations (also known as HTTP methods).
+The path /items/{item_id} has a path parameter item_id that should be an int.
+The path /items/{item_id} has an optional str query parameter q.
+'''
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {
